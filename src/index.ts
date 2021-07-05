280b497a5352bc9c48a19b13023acb9ab7052259
callisto1337 <hazardous333@gmail.com>
2021-07-06 02:19:04 +0300

    first commit

---
+++ src/index.ts
@@ -0,0 +1,22 @@
+const rect = {
+  perimeter: (x: number, y: number) => 2 * (x + y),
+  area: (x: number, y: number) => x * y,
+};
+
+function solveRectangle(l: number, b: number) {
+  console.log(`Solving or rectangle with = ${l}, b = ${b}`);
+
+  if (l <= 0 || b <= 0) {
+    console.log(`Rectangle dimensions should be greater than zero: l = ${l}, b = ${b}`);
+  } else {
+    console.log(`The area of the rectangle is ${rect.area(l, b)}`);
+    console.log(`The perimeter of the rectangle is ${rect.perimeter(l, b)}`);
+  }
+
+  console.log(`\n`);
+}
+
+solveRectangle(2, 4);
+solveRectangle(3, 5);
+solveRectangle(0, 5);
+solveRectangle(-3, 5);
